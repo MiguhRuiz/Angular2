623ca39c704d04c150a77f20e10a6150c81afac4
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-09 18:08:32 +0100

    [Fix] Folder position error

--- angular/app/components/coursebox.component.ts
+++ angular/app/components/coursebox.component.ts
@@ -2,7 +2,7 @@
  * Created by miguhruiz on 9/12/16.
  */
 import { Component, Input } from '@angular/core'
-import { Course } from '../common/course'
+import { Course } from '../../common/course'
 
 @Component({
     selector: 'coursebox',
